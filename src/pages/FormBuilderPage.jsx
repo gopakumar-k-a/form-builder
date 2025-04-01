51a80248296b155fc70bf9e05a600ab64e11865e
gopakumar-k-a <gopak9145@gmail.com>
2025-04-01 11:58:41 +0530

    feat: Update left sidebar to include all draggable elements

--- src/pages/FormBuilderPage.jsx
+++ src/pages/FormBuilderPage.jsx
@@ -4,19 +4,21 @@ import RightSideBarButton from "../components/buttons/roundedButons/RightSideBar
 import LeftSidebar from "../components/sideBar/LeftSideBar";
 import RightSidebar from "../components/sideBar/RightSideBar";
 import Canvas from "../components/canvas/Canvas";
-import { useSelector, useDispatch } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 import { setSelectedComponent } from "../redux/reducers/canvas/canvasReducer";
+import useCanvas from "../hooks/useCanvas";
 function FormBuilderPage() {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
+  const {setSelectedComponent,selectedComponent}=useCanvas()
   const [isLeftSideBarOpen, setLeftSideBarOpen] = useState(false);
   const [isRightSideBarOpen, setRightSideBarOpen] = useState(false);
-  const { selectedComponent } = useSelector((state) => state.canvas);
+  // const { selectedComponent } = useSelector((state) => state.canvas);
   const handleLeftSideBarToggle = () => {
     setLeftSideBarOpen((prev) => !prev);
   };
   const handleRightSideBarToggle = () => {
     setRightSideBarOpen((prev) => !prev);
-    dispatch(setSelectedComponent({ operation: "remove" }));
+setSelectedComponent({ operation: "remove" })
   };
   useEffect(() => {
     selectedComponent ? setRightSideBarOpen(true) : setRightSideBarOpen(false);
